52f91f0b50d2cf5a14b25154aae39231922ade78
Aniket Chitransh <ani.chitransh@gmail.com>
2024-07-16 14:32:02 +0530

    Added the title and tag line for the application

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,15 @@ export default function Home() {
     <div>
       <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-      <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
-        Backgrounds
-      </p>
-    </div>
+        <div className="relative overflow-hidden h-screen">
+          <div className="max-w-[85rem] mx-auto px-4 sm:px-6 py-10 sm:py-24">
+            <div className="text-center">
+              <h1 className="text-4xl sm:text-6xl font-bold text-neutral-200">Convert <span className="text-emerald-500">Lang</span></h1>
+              <p className="mt-3 text-neutral-400">Convert Lang: Instant Language Translation</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
