723f8f1f8e364710a9be0b2a1b61d09247523168
Aniket Chitransh <ani.chitransh@gmail.com>
2024-07-16 17:28:36 +0530

    Created an Input Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
+'use client'
+
 import Image from "next/image";
+import React, { useState, ChangeEvent } from 'react';
+
+import TextArea from '@/components/inputs/TextArea'
 
 export default function Home() {
+  const [sourceText, setSourceText] = useState<string>("");
+
   return (
     <div>
       <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
@@ -10,6 +17,21 @@ export default function Home() {
             <div className="text-center">
               <h1 className="text-4xl sm:text-6xl font-bold text-neutral-200">Convert <span className="text-emerald-500">Lang</span></h1>
               <p className="mt-3 text-neutral-400">Convert Lang: Instant Language Translation</p>
+
+              <div className="mt-7 sm:mt-12 mx-auto max-w-3xl relative">
+                <div className="grid gap-4 md:grid-cols-2 grid-cols-1">
+                  <div className="relative z-10 flex flex-col space-x-3 border rounded-lg shadow-lg bg-neutral-900 border-neutral-700 shadow-gray-900/20">
+                    <TextArea
+                      id='source-language'
+                      value={sourceText}
+                      onChange={(e: ChangeEvent<HTMLTextAreaElement>) => {
+                        setSourceText(e.target.value)
+                      }}
+                      placeholder='Source Language'
+                    />
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
