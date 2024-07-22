47efd7f88ae8f60632bb000ca77795754d4f3483
Aniket Chitransh <ani.chitransh@gmail.com>
2024-07-22 19:05:45 +0530

    Created a custome hook useTranslate

--- src/hooks/useTranslate.jsx
+++ src/hooks/useTranslate.jsx
@@ -5,3 +5,43 @@ const openai = new OpenAI({
     apiKey: process.env.loacl.OPENAI_API_KEY,
     dangerouslyAllowBrowser: true
 })
+
+const useTranslate = (sourceText, selectedLanguage) => {
+    const [targetText, setTargetText] = useState();
+
+    useEffect( () => {
+        const handleTranslate = async (sourceText) => {
+            try {
+                const response = await openai.chat.completions.create({
+                    model: 'gpt-4o',
+                    messages: [{
+                        role: 'user',
+                        content:
+                            `
+                                You will be provided with a sentence. This sentence: ${sourceText}. Your task is to: 
+                                - detect what language the sentence is written in
+                                - translate the sentence into ${selectedLanguage}
+                                do not return anything other than the translated sentence
+                            `
+                    }]
+                })
+                const data = response.choices[0].message.content
+                setTargetText(data)
+            } catch (error) {
+                console.log('Error translating text:', error);
+            }
+        }
+
+        if (sourceText.trim()) {
+            const timeoutId = setTimeout(() => {
+                handleTranslate(sourceText)
+            }, 500)
+
+            return () => clearImmediate(timeoutId)
+        }
+    }, [sourceText, selectedLanguage])
+
+    return targetText;
+}
+
+export default useTranslate;
